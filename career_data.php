e0f5a676f28040071be5303c2a2ed665dc73d346
Shutnik89 <shutnik89@gmail.com>
2018-12-20 13:12:55 +0300

    init commit

---
+++ career_data.php
@@ -0,0 +1,81 @@
+<?php
+
+    return  [
+        'about' => 'Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.',
+        'education' => [
+            [
+            'title' => 'Universitet',
+            'speciality' => 'Student',
+            'yearStart' => '2006',
+            'yearEnd' => '2012'
+            ],
+            [
+            'title' => 'Universitet',
+            'speciality' => 'Student',
+            'yearStart' => '2006',
+            'yearEnd' => '2012'
+            ]
+        ],
+        'experiences' => [
+            [
+            'title' => 'Place work #1',
+            'post' => 'Post #1',
+            'company' => 'Company 1',
+            'city' => 'Minsk',
+            'about' =>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be',
+            'yearStart' => '2012',
+            'yearEnd' => '2014'
+            ],[
+            'title' => 'Place work #2',
+            'post' => 'Post #2',
+            'company' => 'Company 2',
+            'city' => 'Minsk',
+            'about' =>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be',
+            'yearStart' => '2014',
+            'yearEnd' => '2016'
+            ],[
+            'title' => 'Place work #3',
+            'post' => 'Post #3',
+            'company' => 'Company 3',
+            'city' => 'Minsk',
+            'about' =>'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be',
+            'yearStart' => '2016',
+            'yearEnd' => 'Present'
+            ],
+
+        ],
+        'Projectes' => [
+           [
+               'title' => 'Project 1',
+               'link' => 'example1.com',
+               'about' => 'orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be',
+           ],
+           [
+               'title' => 'Project 2',
+               'link' => 'example2.com',
+               'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has beorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be',
+           ],
+           [
+               'title' => 'Project 3',
+               'link' => 'example2.com',
+               'about' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has beorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has beorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has beorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has be',
+           ],
+        ],
+        'Skils' => [
+            [
+                'title' => 'PHP',
+                'level' => '10%',
+            ],
+            [
+                'title' => 'HTML 5',
+                'level' => '50%',
+            ],
+            [
+                'title' => 'CSS 3',
+                'level' => '50%',
+            ],
+        ],
+
+    ];
+
+?>
---
+++ profile_data.php
@@ -0,0 +1,41 @@
+<?php
+
+    return [
+        'about' => [
+            'Name' => 'Denis Sycheuski',
+            'Photo' => '',
+            'Email' => 'denissycheuski@gmail.com',
+            'Phone' => '375 44 123 45 67',
+            'Site' => 'Example.com',
+            'Post' => ' Junior Developer PHP'
+        ],
+        'education' => [
+            [
+            'title' => 'School',
+            'speciality' => 'Student',
+            'yearStart' => '1995',
+            'yearEnd' => '2006'
+            ],
+            [
+            'title' => 'Universitet',
+            'speciality' => 'Student',
+            'yearStart' => '2006',
+            'yearEnd' => '2012'
+            ]
+        ],
+        'Language' => [
+            [
+            'title' => 'English',
+            'level' => 'intermediate'
+            ],
+            [
+             'title' => 'Russian',
+             'level' => 'Professianal'
+            ]
+        ],
+        'interesets' => [
+            'Bicycle',
+            'Learn language'
+        ]
+    ];
+?>
